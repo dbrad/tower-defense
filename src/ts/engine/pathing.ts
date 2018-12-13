9cec3951aef6f262b8e6482234f9a60d0566ea4c
David <davidbrad.015@gmail.com>
2018-12-13 07:44:04 -0500

    started a* pathfinding work

---
+++ src/ts/engine/pathing.ts
@@ -0,0 +1,58 @@
+namespace Engine {
+    export namespace Pathing {
+
+        export class CellMap {
+            cells: Cell[];
+            size: V2;
+        }
+
+        export class Cell {
+            parent: Cell;
+            position: V2;
+            g: number;
+            h: number;
+            get f() {
+                return this.g + this.h;
+            }
+        }
+
+        type NeighborGroup = {
+            North: Cell,
+            South: Cell,
+            West: Cell,
+            East: Cell,
+            NorthWest: Cell,
+            NorthEast: Cell,
+            SouthWest: Cell,
+            SouthEast: Cell
+        }
+
+        function GetNeighbors(map: CellMap, cell: Cell): NeighborGroup {
+            let neighbors: NeighborGroup;
+            let positionCopy = CopyV2(cell.position);
+            neighbors.North = getCell(map, V2.add(positionCopy, { x: 1, y: 0 }));
+            neighbors.North = getCell(map, V2.add(positionCopy, { x: 1, y: 0 }));
+            neighbors.North = getCell(map, V2.add(positionCopy, { x: 1, y: 0 }));
+            neighbors.North = getCell(map, V2.add(positionCopy, { x: 1, y: 0 }));
+
+            neighbors.North = getCell(map, V2.add(positionCopy, { x: 1, y: 0 }));
+            neighbors.North = getCell(map, V2.add(positionCopy, { x: 1, y: 0 }));
+            neighbors.North = getCell(map, V2.add(positionCopy, { x: 1, y: 0 }));
+            neighbors.North = getCell(map, V2.add(positionCopy, { x: 1, y: 0 }));
+            return neighbors;
+        }
+
+        function getCell(map: CellMap, position: V2): Cell {
+            if (position.x < 0 || position.x >= map.size.x ||
+                position.y < 0 || position.y >= map.size.y) {
+                return null;
+            }
+            let index = position.x + (position.y * map.size.x);
+            return map.cells[index];
+        }
+
+        export function generate() {
+
+        }
+    }
+}
