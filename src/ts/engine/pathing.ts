babdbe4d9d338dd45ac463b25ae20b7834ec235e
David <davidbrad.015@gmail.com>
2018-12-14 07:51:32 -0500

    Started work on generate path call.

--- src/ts/engine/pathing.ts
+++ src/ts/engine/pathing.ts
@@ -1,4 +1,14 @@
+/// <reference path="entity.ts" />
+/// <reference path="tilemap.ts" />
+/// <reference path="util.ts" />
+
 namespace Engine {
+    export namespace TileMap {
+        export function convertToCellMap(tileMap: Engine.TileMap, entityManager: ECS.Manager): Pathing.CellMap {
+            return new Pathing.CellMap();
+        }
+    }
+
     export namespace Pathing {
 
         export class CellMap {
@@ -16,19 +26,31 @@ namespace Engine {
             }
         }
 
-        type NeighborGroup = {
-            North: Cell,
-            South: Cell,
-            West: Cell,
-            East: Cell,
-            NorthWest: Cell,
-            NorthEast: Cell,
-            SouthWest: Cell,
-            SouthEast: Cell
+        class NeighborGroup {
+            North: Cell;
+            South: Cell;
+            West: Cell;
+            East: Cell;
+            NorthWest: Cell;
+            NorthEast: Cell;
+            SouthWest: Cell;
+            SouthEast: Cell;
+
+            *[Symbol.iterator](): IterableIterator<Cell> {
+                yield this.North;
+                yield this.South;
+                yield this.West;
+                yield this.East;
+                yield this.NorthWest;
+                yield this.NorthEast;
+                yield this.SouthWest;
+                yield this.SouthEast;
+                return (1);
+            }
         }
 
-        function GetNeighbors(map: CellMap, cell: Cell): NeighborGroup {
-            let neighbors: NeighborGroup;
+        function getNeighbors(map: CellMap, cell: Cell): NeighborGroup {
+            let neighbors: NeighborGroup = new NeighborGroup();
             let positionCopy = CopyV2(cell.position);
             neighbors.North = getCell(map, V2.add(positionCopy, { x: 0, y: -1 }));
             neighbors.South = getCell(map, V2.add(positionCopy, { x: 0, y: 1 }));
@@ -51,8 +73,23 @@ namespace Engine {
             return map.cells[index];
         }
 
-        export function generate() {
+        export function generatePath(map: CellMap, start: V2, end: V2): V2[] {
+            let openList: Cell[] = [];
+            let closedList: Cell[] = [];
 
+            let startingNode = getCell(map, start);
+            openList.push(startingNode);
+
+            while (openList.length > 0) {
+                openList.sort((a,b) => { return b.f - a.f});
+                let current = openList.pop();
+                let neighbors = getNeighbors(map, current);
+                for(let cell of neighbors) {
+                    
+                }
+                closedList.push(current);
+            }
+            return [];
         }
     }
 }
